d4b3a27afb2329e0b07fd1329eca959f516d11ac
Rob Galanakis <rob.galanakis@gmail.com>
2015-04-21 23:53:49 -0700

    Added stub files

---
+++ app.rb
@@ -0,0 +1,6 @@
+
+class PlaceholderPleaseDelete
+	def self::does_it_work?
+		return true
+	end
+end
---
+++ specs.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+
+require_relative 'app'
+
+
+describe 'the programming test' do
+	it 'works on my machine!' do
+		expect( PlaceholderPleaseDelete.does_it_work? ).to eq( true )
+	end
+end
